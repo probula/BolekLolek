7f9f3d6c2de9d21d2320be7d9cc7ced0a4be4a0d
Igor <igor.kowalczyk@cyfroweszkoly.pl>
2025-09-04 14:25:35 +0200

    text from ListBox zadanieLista adding text to list

--- BolekLolek/MainWindow.axaml.cs
+++ BolekLolek/MainWindow.axaml.cs
@@ -1,11 +1,28 @@
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace BolekLolek;
 
 public partial class MainWindow : Window
 {
+    private ObservableCollection<string> zadania = new ObservableCollection<string>();
+
     public MainWindow()
     {
         InitializeComponent();
+        zadanieLista.ItemsSource = zadania;
+        zadanieButton.Click += DodajZadanie;
+    }
+
+    private void DodajZadanie(object? sender, RoutedEventArgs e)
+    {
+        string zadanie = zadanieDodaj.Text?.Trim() ?? "";
+
+        if (!string.IsNullOrWhiteSpace(zadanie))
+        {
+            zadania.Add(zadanie);
+            zadanieDodaj.Text = "";
+        }
     }
 }
