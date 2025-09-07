2cb3208a70783b20a42274bef4c45faf3cf40cd5
igor <igor.kowalczyk07@poczta.fm>
2025-09-07 21:51:36 +0200

    after clicking the button summary appears

--- BolekLolek/MainWindow.axaml.cs
+++ BolekLolek/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -6,6 +7,7 @@ namespace BolekLolek;
 
 public partial class MainWindow : Window
 {
+    
     private ObservableCollection<string> zadania = new ObservableCollection<string>();
 
     public MainWindow()
@@ -17,12 +19,41 @@ public partial class MainWindow : Window
 
     private void DodajZadanie(object? sender, RoutedEventArgs e)
     {
-        string zadanie = zadanieDodaj.Text?.Trim() ?? "";
+        string zadanie = zadanieDodaj.Text;
+        var wykonawca = (wykonawcaComboBox.SelectedItem as ComboBoxItem).Content.ToString();
+        string priorytet = "";
+        if (rb1.IsChecked == true)
+        {
+            priorytet = "niski";
+        }
+        else if (rb2.IsChecked == true)
+        {
+            priorytet = "normalny";
+        }
+        else if (rb3.IsChecked == true)
+        {
+            priorytet = "wysoki";
+        }
+
+        bool naDworze = NaDworze.IsChecked ?? false;
+        bool sprzet = Sprzet.IsChecked ?? false;
+        bool pomoc = Koledzy.IsChecked ?? false;
+        
+        List<string> zaznaczoneCB = new List<string>();
+        
+        if (naDworze) zaznaczoneCB.Add("na dworze");
+        if (sprzet) zaznaczoneCB.Add("sprzet");
+        if (pomoc) zaznaczoneCB.Add("potrzebna pomoc kolegów");
+        
+        string opcjeCB = string.Join(", ", zaznaczoneCB);
+        
 
         if (!string.IsNullOrWhiteSpace(zadanie))
         {
-            zadania.Add(zadanie);
-            zadanieDodaj.Text = "";
+            string nazwaZ = nazwaZadania.Text;
+
+            string tekst = $"{wykonawca} - {nazwaZ}, priorytet: {priorytet}. Dodatkowe informacje: {opcjeCB}";
+            zadania.Add(tekst);
         }
     }
 }
